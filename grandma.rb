05c6370f1eb65c25f7af631e0da53f9a6ddb903f
Lillian Zhang <lilyzhangyun@gmail.com>
2018-05-21 13:09:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,6 +3,8 @@
 def speak_to_grandma(phrase)
     if phrase != phrase.upcase()
       return "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == "I LOVE YOU GRAMMA"
+      return "I LOVE YOU TOO"
     else
       return "NO, NOT SINCE 1938!"
     end
